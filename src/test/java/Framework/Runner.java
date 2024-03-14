80230e2ca4376eb254094d346bfb6f634cb7050f
Deepak Nayal <deepak.nayal@practicenumbers.com>
2024-03-14 01:12:56 -0700

    PR comments resolved

--- src/test/java/Framework/Runner.java
+++ src/test/java/Framework/Runner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         plugin = {"pretty","html:target/HtmlReports/testResult.html",
                 "json:target/JSONReports/testResult.json",
                 "junit:target/XMLReports/testResult.xml"},
-        tags = "@Campaigns")
+        tags = "@Campaigns or @home or @login")
 public class Runner {
 }
 
--- src/test/java/Pages/PracticeHomePage.java
+++ src/test/java/Pages/PracticeHomePage.java
@@ -1,5 +1,4 @@
 package Pages;
-
 import Framework.Browser;
 import Framework.Constants.Constants;
 import Framework.Util.DriverManager;
@@ -14,14 +13,10 @@ public class PracticeHomePage extends BasePage{
     private WebElement userProfileDropdown = DriverManager.getInstance().Driver.findElement(By.xpath("//li[contains(@class,'dropdown account-dropdown')]/a/i[@class='fa fa-caret-down']"));
     private WebElement campaignsMenuItem = DriverManager.getInstance().Driver.findElement(By.xpath("//ul[@class='dropdown-menu']//i[@class='fa fa-list fa-fw']"));
 
-
     public void clickOnUserProfileDropdown() {
         Browser.clickOnElement(userProfileDropdown);
     }
 
-    public void
-            () {
-
-        Browser.clickOnElement(campaignsMenuItem);
+    public void clickOnCampaignsMenuItem() {Browser.clickOnElement(campaignsMenuItem);
     }
 }
