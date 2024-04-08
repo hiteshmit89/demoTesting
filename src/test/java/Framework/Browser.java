5b455607807b0e5cd4611deb3937ed3171478490
Hitesh_Bansal1 <hitesh.bansal@practicenumbers.com>
2024-04-08 12:31:21 +0530

    Commit for PUA-42

--- src/test/java/Framework/Browser.java
+++ src/test/java/Framework/Browser.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
