8f22a0e3d59a237c2346be86a4fcd28875b70f16
Hitesh_Bansal1 <hitesh.bansal@practicenumbers.com>
2024-04-16 16:03:36 +0530

    changes for fixing sheduler appointment script

--- src/test/java/Pages/Modals/CreateAppointmentModal.java
+++ src/test/java/Pages/Modals/CreateAppointmentModal.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 public class CreateAppointmentModal {
-    private WebElement modalTitle = DriverManager.getInstance().Driver.findElement(By.xpath("/html/body//div/h[@class='modal-title']"));
     private List<WebElement> appointmentCalendar = DriverManager.getInstance().Driver.findElements(By.xpath("//*[@id='chat-box']//div[@class='rbc-events-container']"));
 
 
