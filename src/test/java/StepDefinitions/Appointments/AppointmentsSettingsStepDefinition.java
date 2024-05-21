39ac644bb4599510c7983de13230a5eec6238e33
Hitesh_Bansal1 <hitesh.bansal@practicenumbers.com>
2024-05-21 11:36:25 +0530

    Commit for PUA-65

--- src/test/java/StepDefinitions/Appointments/AppointmentsSettingsStepDefinition.java
+++ src/test/java/StepDefinitions/Appointments/AppointmentsSettingsStepDefinition.java
@@ -87,4 +87,17 @@ public class AppointmentsSettingsStepDefinition {
     public void iWillBeAbleToSeeAppointmentsDetailsOnPageList() {
         PbNUIApp.appointmentsPage().clickOnExistingPatient();
     }
+
+    @When("I try to sort columns of Appointment list table on appointment list page")
+    public void iTryToSortColumnsOfAppointmentListTableOnAppointmentListPage() {
+        PbNUIApp.navigator().NavigateTo(Constants.Destination.Appointments);
+        PbNUIApp.appointmentsPage().clickOnAppointmentListTab();
+        PbNUIApp.appointmentsPage().enterStartDate1("01/01/2024");
+        PbNUIApp.appointmentsPage().enterStartDate2("01/01/2024");
+    }
+
+    @Then("I will be able to sort columns of Appointment list table on appointment list page")
+    public void iWillBeAbleToSortColumnsOfAppointmentListTableOnAppointmentListPage() {
+        PbNUIApp.appointmentsPage().clickOnSortTableButton();
+    }
 }
