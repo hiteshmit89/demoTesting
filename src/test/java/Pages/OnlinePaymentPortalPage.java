a8cef793fc801059cd50bf97f9c452fb5c63e126
Vikas Tomar <vikas.tomar@practicenumbers.com>
2024-05-22 12:39:10 +0530

    resolved comments

--- src/test/java/Pages/OnlinePaymentPortalPage.java
+++ src/test/java/Pages/OnlinePaymentPortalPage.java
@@ -94,22 +94,11 @@ public class OnlinePaymentPortalPage extends BasePage {
         WebElement cardCVCID = DriverManager.getInstance().Driver.findElement(By.xpath("//input[@placeholder='3 digits']"));
         Browser.enterTextInEditBox(cardCVCID, cardCVC);
         Browser.switchToDefaultContent();
-
     }
 
     public void enterBillingAddress() {
-        /*WebElement countryDropdown = DriverManager.getInstance().Driver.findElement(By.xpath("//input[@placeholder='Search...']"));
-        Browser.enterTextInEditBox(countryDropdown,PbNUIApp.userdata().getCountry(2,"1"));
-        WebElement countryOption = DriverManager.getInstance().Driver.findElement(By.xpath("//span[@class='adyen-checkout__dropdown__element__text' and contains(text(),'"+PbNUIApp.userdata().getCountry(2,"1")+"')]"));
-        Browser.clickOnElement(countryOption);*/
-        //WebElement streetTextBox = DriverManager.getInstance().Driver.findElement(By.xpath("//input[@name='street']"));
-        //Browser.enterTextInEditBox(streetTextBox,PbNUIApp.userdata().getStreet(2,"1"));
-        //WebElement houseNumberTextBox = DriverManager.getInstance().Driver.findElement(By.xpath("//input[@name='houseNumberOrName']"));
-        //Browser.enterTextInEditBox(houseNumberTextBox,PbNUIApp.userdata().getHouseNumber(2,"1"));
         WebElement postalCodeTextBox = DriverManager.getInstance().Driver.findElement(By.xpath("//input[@name='postalCode']"));
         Browser.enterTextInEditBox(postalCodeTextBox,PbNUIApp.userdata().getPostalCode(2,"1"));
-       // WebElement cityTextBox = DriverManager.getInstance().Driver.findElement(By.xpath("//input[@name='city']"));
-        //Browser.enterTextInEditBox(cityTextBox,PbNUIApp.userdata().getCity(2,"1"));
     }
 
     public void clickPayNow(){
@@ -124,7 +113,5 @@ public class OnlinePaymentPortalPage extends BasePage {
         WebElement paymentLabel = DriverManager.getInstance().Driver.findElement(By.xpath("//p[@class='MuiTypography-root card-title MuiTypography-body1' and contains(text(),'Payment Success!')]"));
         String Label = paymentLabel.getText();
         Assert.assertEquals(Label,"Payment Success!");
-
-
     }
 }
