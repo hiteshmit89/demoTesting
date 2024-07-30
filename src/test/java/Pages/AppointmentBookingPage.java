cb1a80f62a135209c2debd3780a5cd5d3fd026a9
Hitesh_Bansal1 <hitesh.bansal@practicenumbers.com>
2024-07-30 17:11:09 +0530

    fix

--- src/test/java/Pages/AppointmentBookingPage.java
+++ src/test/java/Pages/AppointmentBookingPage.java
@@ -143,6 +143,7 @@ public class AppointmentBookingPage extends BasePage {
     }
 
     public void enterFirstName(String firstNameData) {
+        Browser.waitForPresenceOfElement(By.xpath("//div[@class='patient-form-title']"));
         Browser.waitForElementToBeVisible(By.xpath("//div[@class='patient-form-title']"));
         Browser.waitForElementToBeVisible(By.xpath("//input[@placeholder='Patient First Name']"));
         WebElement firstNameID = DriverManager.getInstance().Driver.findElement(By.xpath("//input[@placeholder='Patient First Name']"));
